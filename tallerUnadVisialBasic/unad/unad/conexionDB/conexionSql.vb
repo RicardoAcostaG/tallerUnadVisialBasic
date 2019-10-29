a7c7639f542cdc4bbbdfb65728feaac1671e42bc
RicardoAcostaGonzalez <ricardo02384@gmail.com>
2019-10-28 22:05:40 -0500

    lineas bases

---
+++ tallerUnadVisialBasic/unad/unad/conexionDB/conexionSql.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Imports System.Windows.Forms
+
+
+Module conexionSql
+
+    'cadena para nectarse a la base de datos por la autenticacion de windows'
+    Public conexion As New SqlConnection("Data Source = RICARDO;Initial Catalog=dbUnad;Integrated Security=True")
+
+    'para abrir la conexion a la base de datos'
+    Sub abrir()
+        If conexion.State = 0 Then
+            conexion.Open()
+        End If
+    End Sub
+
+    'para cerrar la conexion a la base de datos'
+    Sub cerrar()
+        If conexion.State = 1 Then
+            conexion.Close()
+        End If
+    End Sub
+
+End Module
